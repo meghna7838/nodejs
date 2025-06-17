4fde698fbaa23387fbf92ed8031e276095460126
meghna7838 <meghnatandon7838@gmail.com>
2025-06-17 21:44:02 +0530

    Lets start node

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require("./xyz");
+require("./sum")
 var name = "Namaste Node JS";
 var a =10;
 var b=20;
-console.log(name);
-console.log(a+b);
+// console.log(name);
+// console.log(a+b);
 
-console.log(globalThis);
+// console.log(globalThis);
 
-console.log(this);
-console.log(global);
+// console.log(this);
+// console.log(global);
+
+CalculateSum(a,b);
 
---
+++ sum.js
@@ -0,0 +1,6 @@
+console.log("Sum module");
+
+function CalculateSum(a,b){
+    const sum = a+b;
+    console.log(sum);
+}
