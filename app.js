7d681cf1c4bc6aaef4d4819619028267368cc737
meghna7838 <meghnatandon7838@gmail.com>
2025-06-19 21:34:39 +0530

    ES MOdule

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-require("./xyz");
-require("./sum")
+//require("./xyz");
+
+//const {x1,CalculateSum1} = require("./sum")
+//const Sum = require("./sum")
+
+import {x,CalculateSum} from "./sum.js";
 var name = "Namaste Node JS";
 var a =10;
 var b=20;
@@ -11,5 +15,8 @@ var b=20;
 // console.log(this);
 // console.log(global);
 
+//CalculateSum(a,b);
 CalculateSum(a,b);
+console.log("x");
+// console.log(x1)
 
--- sum.js
+++ sum.js
@@ -1,6 +1,12 @@
 console.log("Sum module");
-
-function CalculateSum(a,b){
+export const x=6;
+export function CalculateSum(a,b){
     const sum = a+b;
     console.log(sum);
-}
+};
+
+//module.exports = CalculateSum
+//module.exports = {
+//    x1:x,
+//    CalculateSum1:CalculateSum
+//};
